ed90025d922bc152797986defa2df2ddebc131f7
Thales-Rangel <thalesran2006@gmail.com>
2025-04-29 20:44:27 -0300

    Estrutura de modelo de usuario basico

---
+++ src/main/java/com/thales_dev/e_commerce/models/UsuarioModel.java
@@ -0,0 +1,57 @@
+package com.thales_dev.e_commerce.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class UsuarioModel {
+
+	@Id
+	private String cpf;
+	
+	private String nome;
+	private String email;
+	private String senha;
+	private String endereco;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
